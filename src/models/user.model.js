0b20e839b5ed46c3e68b4cb3b2b545627b562a2d
sunil kushwaha <sunilkushwaha066@hmail.com>
2025-02-06 22:43:56 +0530

    user schema done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -30,5 +30,46 @@ const userSchema = new Schema({
         ref: "User"
     },  // If the user is a student, link their parent
 
-}, { timestamps: true })
+}, { timestamps: true });
+
+// password bcrypt
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+});
+// password compare to login time method
+userSchema.methods.isPasswordCorrect = async function (password) {
+    // password compare
+    return await bcrypt.compare(password, this.password)
+}
+// create access token method
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName,
+        role: this.role,
+
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        })
+}
+// create request token method
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id
+
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        })
+}
+
 export const User = mongoose.model("Users", userSchema)
