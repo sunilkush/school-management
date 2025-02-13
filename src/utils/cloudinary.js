c9dbdd8f17295518ad4239e9baff617ce2c7ff20
Sunil kushwaha <33691873+sunilkush@users.noreply.github.com>
2025-02-13 11:48:51 +0530

    Update cloudinary.js
    resolve cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,8 +6,8 @@ import fs from "fs"
 dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
