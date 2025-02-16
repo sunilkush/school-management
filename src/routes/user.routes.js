97bcb27e9bf1bf7648236d7c599285901e77e24e
sunil kushwaha <sunilkushwaha066@hmail.com>
2025-02-16 18:00:07 +0530

    add rotue login, change password

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,9 @@ import { Router } from "express"
 import { auth, } from "../middlewares/auth.middleware.js";
 import {
     registerUser,
-    updateUser
+    loginUser,
+    updateUser,
+    changeCurrentPassword
 } from "../controllers/user.controllers.js"
 import { upload } from "../middlewares/multer.middleware.js"
 const router = Router()
@@ -11,6 +13,11 @@ router.route('/register').post(upload.fields([{
     name: "",
     maxCount: 1
 }]), registerUser)
+
+router.route('/login').post(loginUser)
+
+// secure route
 router.route('/update/:id').post(auth, updateUser)
+router.route('/changePassword').post(auth, changeCurrentPassword)
 
 export default router
