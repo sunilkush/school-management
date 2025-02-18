ad6845ae48ea5ebc204abddd37eb8d0085cb89e4
Sunil kushwaha <33691873+sunilkush@users.noreply.github.com>
2025-02-18 11:21:40 +0000

    add frontend

---
+++ backend/src/routes/school.routes.js
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import {
+    schoolRegister,
+    schoolUpdate,
+    deactivateSchool,
+} from '../controllers/school.controllers.js';
+import { upload } from '../middlewares/multer.middleware.js';
+import { auth, roleMiddleware } from '../middlewares/auth.middleware.js';
+
+const router = Router()
+
+router.route('/register').post(
+    auth,
+    roleMiddleware(['Admin', 'Super Admin']),
+    upload.fields([
+        {
+            name: 'logo',
+            maxCount: 1,
+        },
+    ]),
+    schoolRegister
+)
+
+router.route('/update/:id').post(
+    auth,
+    roleMiddleware(['Admin']),
+    upload.fields([
+        {
+            name: 'logo',
+            maxCount: 1,
+        },
+    ]),
+    schoolUpdate
+)
+
+router
+    .route('/deactivate/id')
+    .post(auth, roleMiddleware(['Admin', 'Super Admin']), deactivateSchool)
+
+export default router
