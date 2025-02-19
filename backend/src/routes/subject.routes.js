3bcc0be6ccee390e85cf0409ec8127e4628d0018
Sunil kushwaha <33691873+sunilkush@users.noreply.github.com>
2025-02-19 12:53:31 +0000

    add routes

--- backend/src/routes/subject.routes.js
+++ backend/src/routes/subject.routes.js
@@ -7,11 +7,23 @@ import {
     deleteSubject,
 } from '../controllers/subject.controllers.js'
 
-import { auth, roleMiddleware } from '../middlewares/auth.middleware.js'
-const router = Router()
-router.route('/addSubject').post(auth, createSubject)
-router.route("/subjectAll").get(auth, getAllSubjects)
-router.route("/subject/:id").get(auth, getSubject)
-router.route("/update/:id").put(auth, updateSubject)
-router.route('/delete/:id').delete(auth, deleteSubject)
+import { auth, roleMiddleware } from '../middlewares/auth.middleware.js';
+
+const router = Router();
+
+router
+    .route('/addSubject')
+    .post(auth, roleMiddleware('teacher', 'Admin'), createSubject)
+router
+    .route('/subjectAll')
+    .get(auth, roleMiddleware('teacher', 'Admin'), getAllSubjects)
+router
+    .route('/subject/:id')
+    .get(auth, roleMiddleware('teacher', 'Admin'), getSubject)
+router
+    .route('/update/:id')
+    .put(auth, roleMiddleware('teacher', 'Admin'), updateSubject)
+router
+    .route('/delete/:id')
+    .delete(auth, roleMiddleware('teacher', 'Admin'), deleteSubject)
 export default router
