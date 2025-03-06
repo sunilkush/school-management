59d09d49b1899101093727552d885023632715ee
Sunil kushwaha <33691873+sunilkush@users.noreply.github.com>
2025-03-06 08:13:43 +0000

    login api  add

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import google from "../assets/google.svg";
 import facebook from "../assets/facebook.svg";
-
-
+import { useState } from "react";
 
 const LoginPage = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async(e) => {
+        console.log(email)
+        e.preventDefault();
+        setError("");
+        setLoading("");
+
+        try {
+            const response = await fetch("", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, password })
+            })
+            const data = await response.json();
+            setLoading(false);
+            if (!response.data) {
+                throw new Error(data.message || "Login Failed")
+            }
+            // Store token (if needed)
+            localStorage.setItem("token", data.token);
+            alert("Login successful");
+
+        } catch (error) {
+            setError(error.message);
+            setLoading(false)
+        }
+    }
     return (
         <>
             <div className="w-full h-screen bg-gray-900 md:px-40 sm:px-2.5">
@@ -24,8 +55,8 @@ const LoginPage = () => {
 
                         </div>
                         <div className="">
-                            <div>
-
+                            <form >
+                                {error && <p className="text-red-500 mb-4">{error}</p>}
                                 <div className="relative mb-6">
                                     <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                                         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
@@ -33,7 +64,7 @@ const LoginPage = () => {
                                             <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
                                         </svg>
                                     </div>
-                                    <input type="text" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" />
+                                    <input type="text" id="input-group-1" value={email} onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" />
                                 </div>
                                 <div className="relative mb-6">
                                     <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
@@ -42,15 +73,15 @@ const LoginPage = () => {
                                         </svg>
 
                                     </div>
-                                    <input type="text" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" />
+                                    <input type="text" id="input-group-1" value={password} onChange={(e) => setPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" />
                                 </div>
-                            </div>
+                            </form>
                         </div>
                         <div className="">
                             <p className="text-blue-600 text-right">Forgot Password ?</p>
                         </div>
                         <div className="mt-3">
-                            <button className="w-full rounded-lg bg-blue-800 text-white py-3 cursor-pointer hover:bg-blue-950">Login</button>
+                            <button type="button" onClick={handleSubmit} disabled={loading} className="w-full rounded-lg bg-blue-800 text-white py-3 cursor-pointer hover:bg-blue-950">{loading ? "Logging in..." : "Login"}</button>
                         </div>
                         <div className="mt-5">
                             <p className="text-center">Don't have an account? <span className="text-blue-800">Create an account</span></p>
