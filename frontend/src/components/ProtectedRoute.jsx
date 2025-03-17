b76d438fb3c98a95f88d80513da8fc45f926c980
Sunil kushwaha <33691873+sunilkush@users.noreply.github.com>
2025-03-17 05:58:52 +0000

    remove fronted

--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-    const token = localStorage.getItem("token");
-    return token ? children : <Navigate to="/" replace />;
-};
-
-export default ProtectedRoute;
