75d70685c24c60413b172e3a95f477751363ea61
Sunil kushwaha <33691873+sunilkush@users.noreply.github.com>
2025-03-17 10:55:57 +0000

    add frontend

---
+++ frontend/src/main.jsx
@@ -0,0 +1,40 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './App.jsx'
+import store from './store/store.js'
+import { RouterProvider,createBrowserRouter } from 'react-router-dom'
+import AuthLayout from './components/AuthLayout.jsx'
+import Home from './pages/Home.jsx'
+import LoginPage from './pages/Login.jsx'
+const router = createBrowserRouter([{
+  path:"/",
+  element:<App/>,
+  children:[
+    {
+      path:'/',
+      element:(
+      <AuthLayout authentication={false}>
+      <Home/>
+      </AuthLayout>
+      )
+    },
+    {
+      path:'/login',
+      element:(
+      <AuthLayout authentication={false}>
+      <LoginPage/>
+      </AuthLayout>
+      )
+    },
+
+  ]
+
+}])
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+       <RouterProvider router={router} ></RouterProvider>
+  </Provider>
+)
