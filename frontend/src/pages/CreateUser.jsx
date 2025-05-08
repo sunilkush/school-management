50e54bdb03214a2c75e1c02c152490cb40c99b8b
sunil kushwaha <sunilkushwaha066@hmail.com>
2025-05-08 21:46:40 +0530

    error fixed

--- frontend/src/pages/CreateUser.jsx
+++ frontend/src/pages/CreateUser.jsx
@@ -4,7 +4,7 @@ import { fetchRoles } from "../store/roleSlice.js";
 import { fetchSchools } from "../store/schoolSlice.js";
 import { registerUser } from "../store/registerSlice.js";
 import { toast } from "react-toastify";
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 const CreateUser = () => {
   const dispatch = useDispatch();
   const { roles } = useSelector((state) => state.roles || {});
@@ -17,10 +17,10 @@ const CreateUser = () => {
     password: "",
     avatar: "",
     isActive: true,
-    roleId: "",   
-    schoolId: "", 
+    roleId: "",
+    schoolId: "",
   });
-  
+
   useEffect(() => {
     dispatch(fetchRoles());
     dispatch(fetchSchools());
@@ -46,7 +46,7 @@ const CreateUser = () => {
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
-  
+
     if (name === "avatar") {
       setFormData({ ...formData, avatar: files[0] }); // set file object
     } else {
@@ -56,7 +56,7 @@ const CreateUser = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  
+
     const data = new FormData();
     data.append("name", formData.name);
     data.append("email", formData.email);
@@ -67,58 +67,58 @@ const CreateUser = () => {
     if (formData.avatar) {
       data.append("avatar", formData.avatar);
     }
-    
+
     dispatch(registerUser(data));
 
-    
+
 
 
   };
 
   return (
-  <>
-  
-  <div className="min-h-screen flex items-center justify-center bg-blue-gray-50 py-10 px-4">
-      <div className="bg-white shadow-lg rounded-xl w-full max-w-4xl p-8 md:grid md:grid-cols-2 gap-6">
-        <div>
-          <h2 className="text-2xl font-bold text-gray-800 mb-4">Register Admin</h2>
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div>
-              <label className="block text-sm font-medium text-gray-700">Name</label>
-              <input
-                name="name"
-                type="text"
-                placeholder="John Doe"
-                onChange={handleChange}
-                value={formData.name}
-                className="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring-cyan-500 focus:border-cyan-500"
-              />
-            </div>
+    <>
 
-            <div>
-              <label className="block text-sm font-medium text-gray-700">Email</label>
-              <input
-                name="email"
-                type="email"
-                placeholder="john@example.com"
-                value={formData.email}
-                onChange={handleChange}
-                className="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring-cyan-500 focus:border-cyan-500"
-              />
-            </div>
+      <div className="min-h-screen flex items-center justify-center bg-blue-gray-50 py-10 px-4">
+        <div className="bg-white shadow-lg rounded-xl w-full max-w-4xl p-8 md:grid md:grid-cols-2 gap-6">
+          <div>
+            <h2 className="text-2xl font-bold text-gray-800 mb-4">Register Admin</h2>
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-700">Name</label>
+                <input
+                  name="name"
+                  type="text"
+                  placeholder="John Doe"
+                  onChange={handleChange}
+                  value={formData.name}
+                  className="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring-cyan-500 focus:border-cyan-500"
+                />
+              </div>
 
-            <div>
-              <label className="block text-sm font-medium text-gray-700">Password</label>
-              <input
-                name="password"
-                type="password"
-                value={formData.password}
-                onChange={handleChange}
-                className="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring-cyan-500 focus:border-cyan-500"
-              />
-            </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">Email</label>
+                <input
+                  name="email"
+                  type="email"
+                  placeholder="john@example.com"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring-cyan-500 focus:border-cyan-500"
+                />
+              </div>
 
-           
+              <div>
+                <label className="block text-sm font-medium text-gray-700">Password</label>
+                <input
+                  name="password"
+                  type="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  className="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring-cyan-500 focus:border-cyan-500"
+                />
+              </div>
+
+
               <label className="block text-sm font-medium text-gray-700">Role</label>
               <select
                 name="roleId"
@@ -133,51 +133,51 @@ const CreateUser = () => {
                     <option key={role._id} value={role._id}>
                       {role.name}
                     </option>
-                ))}
-            </select>
-
-            <div>
-              <label className="block text-sm font-medium text-gray-700">School</label>
-              <select
-                name="schoolId"
-                value={formData.school}
-                onChange={handleChange}
-                className="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring-cyan-500 focus:border-cyan-500"
-              >
-                <option value="">Select School</option>
-                {schools?.map((school) => (
-                  <option key={school._id} value={school._id}>
-                    {school.name}
-                  </option>
-                ))}
+                  ))}
               </select>
-             
-            </div>
 
-            <div>
-              <label className="block text-sm font-medium text-gray-700">Avatar</label>
-              <input
-                type="file"
-                name="avatar"
-                onChange={handleChange}
-                className="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring-cyan-500 focus:border-cyan-500"
-              />
-            </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">School</label>
+                <select
+                  name="schoolId"
+                  value={formData.school}
+                  onChange={handleChange}
+                  className="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring-cyan-500 focus:border-cyan-500"
+                >
+                  <option value="">Select School</option>
+                  {schools?.map((school) => (
+                    <option key={school._id} value={school._id}>
+                      {school.name}
+                    </option>
+                  ))}
+                </select>
 
-            <button
-              type="submit"
-              className="hidden sm:inline-flex  text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center mr-3"
-              disabled={status === "loading"}
-            >
-              {status === "loading" ? "Registering..." : "Register"}
-            </button>
-            
-          </form>
+              </div>
+
+              <div>
+                <label className="block text-sm font-medium text-gray-700">Avatar</label>
+                <input
+                  type="file"
+                  name="avatar"
+                  onChange={handleChange}
+                  className="w-full mt-1 p-2 border border-gray-300 rounded-lg focus:ring-cyan-500 focus:border-cyan-500"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="hidden sm:inline-flex  text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center mr-3"
+                disabled={status === "loading"}
+              >
+                {status === "loading" ? "Registering..." : "Register"}
+              </button>
+
+            </form>
+          </div>
         </div>
       </div>
-      </div>
-  </>
- );
+    </>
+  );
 };
 
 export default CreateUser;
