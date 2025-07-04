2b8207b46770b631225636d29be99d6b6314ec42
sunilkush <sunilkushwaha066@gmail.com>
2025-07-04 12:36:51 +0530

    add new page

---
+++ frontend/src/pages/schoolAdminReport.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SchoolAdminReport = () => {
+  return (
+    <div>
+       <h1 className='text-2xl font-bold'>School Admin Report</h1>
+       <p className='text-gray-600'>This page will display the report for school admins.</p>
+    </div>
+  )
+}
+
+export default SchoolAdminReport
